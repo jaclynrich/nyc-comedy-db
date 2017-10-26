fd92198f64470c4d34934650c0e84ce1c65b6fe8
Jaclyn Rich <jaclyn.r.rich@gmail.com>
2017-10-25 22:58:26 -0400

    refactor: remove the un-encapsulated version of extract_data function

--- comedy_cellar.py
+++ comedy_cellar.py
@@ -12,71 +12,7 @@ import urllib.request, urllib.parse, urllib.error
 import requests
 from bs4 import BeautifulSoup
 
-#%% Gets all of the show information for one date
-
-url = 'http://www.comedycellar.com/line-up/'
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, "html.parser")
-
-shows = []
-
-# Possible locations for shows
-places = ['MacDougal Street', 'Village Underground', 'Fat Black Pussycat', 'Fat Black Pussycat Lounge']
-
-# This block will be the same for all shows
-day_date = soup.find('div', class_='show-search-title').text.strip()
-ix_space = day_date.find(' ')
-day = day_date[:ix_space]
-date = day_date[ix_space+1:]
-link = soup.find('a', class_='make-comedy-reservation-link')['href']
-
-for gig in soup.findAll('div', class_='show'):
-    show = {}
-    show['day'] = day
-    show['date'] = date
-    show['reservation_link'] = link
-        
-    time_location = gig.find('span', class_='show-time closed').text.strip()
-    time, location = time_location.split('|')
-    show['time'] = time.strip()[:-5]
-    
-    # If location has pm in it it has a show name with the time
-    pm_ix = location.find('pm')
-    if pm_ix > 0:
-        show['show_name'] = location.strip()[pm_ix+2:]
-    else:
-        show['location'] = location.strip()
-    
-    # Sometimes the location is only present in the show note
-    note = gig.find('p', class_='show-note')
-    if note is not None:
-        note = note.text.strip()
-        show['show_note'] = note
-        matching = [place for place in places if place in note]
-        if len(matching) == 1:
-            show['location'] = matching[0]
-            print(show['location'])
-    
-    # If the location is not stated, then it is assumed to be MacDougal Street
-    if show.get('location') == None:
-        show['location'] = 'MacDougal Street'
-        
-    show['performers'] = []
-    
-    for performer in gig.findAll('span', class_='comedian-block-desc-name'):
-        p = ' '.join(performer.getText().strip().split())
-        if(p.startswith('MC')):
-            mc_ix = p.find(':')
-            show['MC'] = p[mc_ix+2:]
-        else:
-            show['performers'].append(p)
-            
-    shows.append(show)
-
-#%% Encapsulate getting data for one page of data into a function
-
-url = 'http://www.comedycellar.com/line-up/'
-
+# Gets all of the show information for one date
 def extract_data(url):
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html, "html.parser")
@@ -135,12 +71,17 @@ def extract_data(url):
     return shows
 
 # Test function out for one page
+url = 'http://www.comedycellar.com/line-up/'
 shows_list = extract_data(url)
 
 #%% Get date options from dropdown menu
 
 date_options = []
 value_options = []
+
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, "html.parser")
+
 options = soup.find('select', class_='dropkick filter-lineup-shows')
 for option in options.findAll('option'):
     date_options.append(option.text.strip()) # just in case it is needed
